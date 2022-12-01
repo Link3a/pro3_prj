c42ce0825ad5cfa8d977f32780b4801ca5f7801c
Link3a <lina_lapa_run@inbox.ru>
2022-12-01 04:44:59 +0300

    Создала БД, Форму глав.окна + добавление

---
+++ main.py
@@ -0,0 +1,38 @@
+import sqlite3
+import sys
+
+from PyQt5.QtWidgets import QApplication, QWidget, QTableWidgetItem
+from Form_staff import Ui_Form
+
+
+class MуWidget(QWidget, Ui_Form):
+    def __init__(self):
+        super(MyWidget, self).__init__()
+        self.setupUi(self)
+        self.pbOpen.clicked.connect(self.open)
+
+    def open(self):
+        try:
+            self.conn = sqlite3.connect('staff_db.db')
+            cur = self.conn.cursor()
+            data = cur.execute("select * from staff")
+            col_name = [i[0] for i in data.description]
+            data_rows = data.fetchall()
+        except Exception as e:
+            print("Ошибки подключением к БД")
+            return e
+        self.twStaffs.setColumnCount(len(col_name))
+        self.twStaffs.setHorizontalHeader(col_name)
+        self.twStaffs.setRowCount(0)
+        for i, row in enumerate(data_rows):
+            self.twStaffs.setRowCount(self.twStaffs.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.twStaffs.setItem(i, j, QTableWidgetItem(str(elem)))
+        self.twStaffs.resizeColumnsToContents()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
